cfef0d9a61747693fee8ec4894e72df8fef67f2f
uthpala abeysinghe <162332672+uthpala99131@users.noreply.github.com>
2025-04-18 18:21:25 +0530

    update

--- nextapp/test/test.js
+++ nextapp/test/test.js
@@ -1,5 +1,5 @@
 var request = require('supertest');
-var app = require('../indexc.js');
+var app = require('../index.js');
 describe('GET /will', function() {
     it('respond with hello world', function(done) {
         request(app).get('/will').expect('{ "response": "Hello World" }', done);
